b420390dda30a2733364a2f277c9a8f6ce89f725
Brian Salemi <briandsalemi@gmail.com>
2020-01-24 21:12:27 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,8 @@
 require 'pry'
 
-def roll_call_dwarves(dwarves)
-  dwarf_list = []
-  dwarves.each_with_index do |dwarf, i|
-    dwarf_list << `#{i} #{dwarf}`
+def roll_call_dwarves(arr)
+  arr.each_with_index do |dwarf, i|
+    puts "#{i + 1}. #{dwarf}"#{dwarf}`
   end
   dwarf_list
 end
